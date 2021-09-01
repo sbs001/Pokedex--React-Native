12f5c0353c4b64fbd5175b3feb2ffa2da52fe46a
Sebastian <tassone_af@yahoo.com>
2021-09-01 18:21:18 -0300

    feat: add sstyles to bottom tab

--- src/navigator/BottomTab.tsx
+++ src/navigator/BottomTab.tsx
@@ -2,14 +2,42 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import SearchScreen from '../screens/SearchScreen/SearchScreen';
 import Navigator from './StackNavigator';
+import { Ionicons } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTab() {
   return (
-    <Tab.Navigator screenOptions={{headerShown:false}}>
-      <Tab.Screen name="Home" component={Navigator} />
-      <Tab.Screen name="Search" component={SearchScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: 'grey',
+        tabBarLabelStyle: { marginBottom: 10 },
+        tabBarStyle: {
+          borderWidth: 0,
+          elevation: 0,
+          height: 60,
+          position: 'absolute',
+          backgroundColor: 'rgba(255,255,255,0.92)'
+        }
+      }}
+      sceneContainerStyle={{ backgroundColor: 'white' }}
+    >
+      <Tab.Screen
+        name="Navigator"
+        component={Navigator}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => <Ionicons color={color} size={25} name={'list-outline'} />
+        }}
+      />
+      <Tab.Screen
+        name="SearchScreen"
+        component={SearchScreen}
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color }) => <Ionicons color={color} size={25} name={'search-outline'} />
+        }} />
     </Tab.Navigator>
   );
 }
